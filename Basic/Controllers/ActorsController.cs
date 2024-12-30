9f2c159e2a13d232caed119c8665b6c0d37c83bd
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2024-12-30 20:11:24 +0530

    Edit and Delete actores

--- Basic/Controllers/ActorsController.cs
+++ Basic/Controllers/ActorsController.cs
@@ -58,10 +58,25 @@ namespace Basic.Controller
             return NoContent();
         }
 
-        [HttpPut]
-        public async Task<ActionResult> Put([FromForm] ActorCreationDTO actorCreationDTO)
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromForm] ActorCreationDTO actorCreationDTO)
         {
-            throw new NotImplementedException();
+            var actor = await context.Actors.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(actor == null)
+            {
+                return NotFound();
+            }
+
+            actor = mapper.Map(actorCreationDTO, actor);
+
+            if(actorCreationDTO.Picture != null) {
+                actor.Picture = await fileStorageService.EditFile(containerName,
+                    actorCreationDTO.Picture, actor.Picture);
+            }
+
+            await context.SaveChangesAsync();
+            return NoContent();
         }
 
         [HttpDelete("{id:int}")]
@@ -73,6 +88,8 @@ namespace Basic.Controller
 
             context.Remove(actor);
             await context.SaveChangesAsync();
+
+            await fileStorageService.DeleteFile(actor.Picture, containerName);
 
             return NoContent();
         }
